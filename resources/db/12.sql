341449b6498bd2f6d7b3623105948ae9ec187576
d-tamm <daniel@tamm-tamm.de>
2020-07-15 21:28:21 +0200

    Add news item

--- resources/db/12.sql
+++ resources/db/12.sql
@@ -1,4 +1,7 @@
 -- Add reference field to bookings
 ALTER TABLE `bookings` ADD `ref` VARCHAR(255) NOT NULL DEFAULT '' AFTER `timestamp`; 
 
+-- Add a news item
+INSERT INTO `news` (`newsId`, `date`, `caption`, `body`) VALUES (NULL, CURRENT_DATE(), 'Bokningsreferens', 'När du lägger en bokning kan du nu lägga till en referens så du lättare kan hålla isär dina bokningar på Min Sida.');
+
 UPDATE config SET value=12 WHERE name='db-version';
