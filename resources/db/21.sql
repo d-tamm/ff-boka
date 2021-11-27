7aeab7259c017ae3858d5c0102a72303cfffc0e6
d-tamm <daniel@tamm-tamm.de>
2021-11-27 12:03:07 +0100

    Cleanup after #322, adjust old DB entries

--- resources/db/21.sql
+++ resources/db/21.sql
@@ -4,4 +4,9 @@ ALTER TABLE `categories`
     CHANGE `accessMember` `accessMember` TINYINT(3) UNSIGNED NULL DEFAULT NULL,
     CHANGE `accessLocal` `accessLocal` TINYINT(3) UNSIGNED NULL DEFAULT NULL; 
 
+-- Set all rows to NULL which until now were set to 0.
+UPDATE categories SET accessExternal=NULL WHERE accessExternal=0;
+UPDATE categories SET accessMember=NULL WHERE accessMember=0;
+UPDATE categories SET accessLocal=NULL WHERE accessLocal=0;
+
 UPDATE config SET value=21 WHERE name='db-version';
